9bc1ef01c78d1d731eca3631bac009cb35e9841c
Sergio <seranmoreno500@gmail.com>
2020-01-22 14:43:05 +0100

    añadido el menú desplegable para los menús
    asd

--- resources/views/map/map.blade.php
+++ resources/views/map/map.blade.php
@@ -24,55 +24,134 @@
     <div id="mapid"></div>
 
     <div id="mapsMenu">
-        @foreach ($maps as $map)
-            <div id="mapTrans{{$map->id}}" class="mapTrans">
-                <!-- The eye and the slider -->
-                <i class="eye fa fa-eye fa-2x"></i> <h2>{{$map->title}}</h2>
-                <br>
-                <!-- same line -->
-                <input type="range" min="1" max="100" value="50" class="slider" id="transparency{{$map->id}}">
-                <span> 50 </span>
-            </div>
-        @endforeach
+        <div id="mapsTrans">
+            <h1> <b>Mapas:</b></h1>
+            @foreach ($maps as $map)
+                <div id="mapTrans{{$map->id}}" class="mapTrans">
+                    <!-- The eye and thr title -->
+                    <div class="contEye">
+                        <i class="eye fa fa-eye fa-2x"></i><h2 class="title">{{$map->title}}</h2>
+                    </div>
+                    <!-- The slider and the -->
+                    <div class="contSlider">
+                        <input type="range" min="0" max="100" value="50" class="slider" id="transparency{{$map->id}}">
+                        <span class="opacity">50</span>
+                    </div>
+                </div>
+            @endforeach
+            <div>
+                <button> Mostrar todos </button>
+                <button> Quitar todos</button>
+            </div>  
+        </div>
+        
+        <br>
+        <div id="mapsShow">
+            <i class="fa fa-chevron-up"></i>
+        </div>
     </div>
+
 
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
     <script>
-    var map = L.map('mapid').setView([36.844092, -2.457840], 14);
+    var map = L.map('mapid', {
+        minZoom: 10,
+        maxZoom: 20
+    });
+
+    map.setView([36.844092, -2.457840], 14);
+    
     
     // Pagina donde están los proveedores de mapas:
     // http://leaflet-extras.github.io/leaflet-providers/preview/index.html
-    var CyclOSM = L.tileLayer('https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+    var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 0,
         maxZoom: 20,
-        attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        ext: 'png'
     });
-    CyclOSM.addTo(map);
+    var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+    });
+    
+    Stamen_TonerLite.addTo(map);
+
+
 
     $(document).ready(function(){
-        $('#mapsMenu').slideToggle(200);
+        $('#mapsMenu').slideToggle(300);
         
         //Eye to enable disable tranparencies
-        $('.eye').click(function() {
-            if($(this).hasClass("fa-eye")){
-                $(this).removeClass("fa-eye");
-                $(this).parent().animate({
-                    opacity: 0.50,
-                }, 100);
-                $(this).addClass("fa-eye-slash");
-                console.log($(this).parent().find("input").attr("disable", true));
-            } else {
-                $(this).removeClass("fa-eye-slash");
-                $(this).addClass("fa-eye");
-                $(this).parent().animate({ 
-                    opacity: 1,
-                }, 100);
-            }
+        $('.contEye').click(function(){
+            swapShowMenu($(this));
         });
 
         //Slider and how it affects the maps
         $('.slider').change(function(){
-            $(this).parent().find("span").text($(this).val());
+            $(this).parent().find(".opacity").text($(this).val());        
         });
+
+        $('#mapsShow').click(function(){
+
+            var icono = $(this).find('i');
+
+            if(icono.hasClass('fa-chevron-up')){
+                $(this).siblings().slideUp(300);
+                icono.removeClass('fa-chevron-up');
+                icono.addClass('fa-chevron-down');
+            } else {
+                $(this).siblings().slideDown(300);
+                icono.removeClass('fa-chevron-down');
+                icono.addClass('fa-chevron-up');
+            }
+            
+        });
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+        // AUXILIAR METHODS //////////////////////////////////////////////////////////////////////
+
+        // SWAP BETWEEN EYES MODES //////////////////////////////////////////////////////////////
+        function swapShowMenu(eyeContainer){
+
+            var eye = jQuery(eyeContainer.find(".eye"));
+
+            if(eye.hasClass("fa-eye")){
+                enable(eyeContainer, eye);
+            } else {
+                disable(eyeContainer, eye);
+            }
+        }
+
+        // ENABLE  //////////////////////////////////////////////////////////////////////////////
+        function enable(eyeContainer, eye){
+            eye.removeClass("fa-eye");
+            eye.parent().animate({
+                opacity: 0.50,
+            }, 100);
+            eye.addClass("fa-eye-slash");
+            eyeContainer.find("input").attr("disabled", true);
+            eyeContainer.siblings('.contSlider').slideUp(200, function(){
+                eyeContainer.siblings().find('.opacity').text(0);
+            });
+        }
+
+        // DISABLE /////////////////////////////////////////////////////////////////////////////
+        function disable(eyeContainer, eye){
+            eye.removeClass("fa-eye-slash");
+            var valueOpacity = eyeContainer.siblings().find('.slider').val();
+            eyeContainer.siblings().find('.opacity').text(valueOpacity);
+            eyeContainer.animate({ 
+                opacity: 1,
+            }, 100);
+            eye.addClass("fa-eye");
+            eyeContainer.find(".opacity").attr("disabled", false);
+            eyeContainer.siblings('.contSlider').slideDown(200);
+        }
     });
 
     </script>
