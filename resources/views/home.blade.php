e933832450f13e2321e5a413f7afe262849f54af
Sergio <seranmoreno500@gmail.com>
2020-02-25 14:15:27 +0100

    empezando el home // Cambios que dijo
    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,26 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Celia Maps</title>
 
-
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <!-- <div class="card-header">Dashboard</div> -->
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Hasta pronto {{ Auth::user()->name }}!
-                </div>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{url('/css/home.css')}}">
+</head>
+<body>
+    <div class="container-fluid text-center">
+        <div class="row bg-primary">
+            <div id="logo" class="col">
+                asd
+            </div>
+        </div>
+        <div class="row">
+            <div  class="col">
+                
             </div>
         </div>
     </div>
-</div>
-@endsection
-
+</body>
+</html>
