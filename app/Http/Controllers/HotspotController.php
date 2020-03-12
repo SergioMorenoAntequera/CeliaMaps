80eac4e9a60d13e4a6df12a0750152a56fe84806
Sergio <seranmoreno500@gmail.com>
2020-03-12 10:29:38 +0100

    Problemas con los hotspots solucionado
    asd

--- app/Http/Controllers/HotspotController.php
+++ app/Http/Controllers/HotspotController.php
@@ -12,7 +12,7 @@ class HotspotController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');     
+        $this->middleware('auth', 'getAllAjax');     
     }
 
     /**
