ac38f1830508d7350ae28c1c1ff9f9a42e2ee526
Sergio <seranmoreno500@gmail.com>
2020-03-12 09:51:20 +0100

    Acceso restringido
    asd

--- app/Http/Controllers/StreetController.php
+++ app/Http/Controllers/StreetController.php
@@ -21,7 +21,7 @@ class StreetController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');     
+        $this->middleware('auth');     
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////
