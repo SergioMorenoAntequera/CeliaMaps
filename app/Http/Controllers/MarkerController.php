a055adda7e37e74999a7508b8b3015799962f977
Sergio <seranmoreno500@gmail.com>
2020-04-08 12:31:10 +0200

    Auths activados
    asd

--- app/Http/Controllers/MarkerController.php
+++ app/Http/Controllers/MarkerController.php
@@ -17,7 +17,7 @@ class MarkerController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth')->except('index', 'show');     
+        $this->middleware('auth')->except('index', 'show');     
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////
