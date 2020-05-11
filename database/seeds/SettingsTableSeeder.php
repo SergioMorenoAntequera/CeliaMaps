a5325886acfe6bbe3ab486dff3dcb2b90a6c09cc
Sergio <seranmoreno500@gmail.com>
2020-05-11 13:27:35 +0200

    Seeder actualizado

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -34,5 +34,43 @@ class SettingsTableSeeder extends Seeder
             'created_at' => date('Y-m-d H-m-s'),
             'updated_at' => date('Y-m-d H-m-s'),
         ]);
+
+        // HOME OPTIONS /////////////////////////////////////////
+        DB::table('settings')->insert([    
+            //La id no se pone porque se autoincrementa sola
+            'name' => "pageTitle",
+            'value' => "Celia Maps",
+            'created_at' => date('Y-m-d H-m-s'),
+            'updated_at' => date('Y-m-d H-m-s'),
+        ]);
+        DB::table('settings')->insert([    
+            //La id no se pone porque se autoincrementa sola
+            'name' => "homeSubtitle",
+            'value' => "Paseo por Almería a través del tiempo",
+            'created_at' => date('Y-m-d H-m-s'),
+            'updated_at' => date('Y-m-d H-m-s'),
+        ]);
+        DB::table('settings')->insert([    
+            //La id no se pone porque se autoincrementa sola
+            'name' => "homeBackground",
+            'value' => "URL DE LA IMAGEN",
+            'created_at' => date('Y-m-d H-m-s'),
+            'updated_at' => date('Y-m-d H-m-s'),
+        ]);
+        DB::table('settings')->insert([    
+            //La id no se pone porque se autoincrementa sola
+            'name' => "description",
+            'value' => "CMS basado en laravel para la superposición de mapas y ver como las ciudades han evolucionado",
+            'created_at' => date('Y-m-d H-m-s'),
+            'updated_at' => date('Y-m-d H-m-s'),
+        ]);
+        
+        DB::table('settings')->insert([    
+            //La id no se pone porque se autoincrementa sola
+            'name' => "keywords",
+            'value' => "cms, laravel, javascript, mapas, superposición de mapas, evolución de ciudades, planos antiguos, developers, programadores",
+            'created_at' => date('Y-m-d H-m-s'),
+            'updated_at' => date('Y-m-d H-m-s'),
+        ]);
     }
 }
