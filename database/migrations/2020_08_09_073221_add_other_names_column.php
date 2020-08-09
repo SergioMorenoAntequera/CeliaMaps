fd103b9d4ed014a03607607a0b16d67b36faf457
Seran <seranmoreno500@gmail.com>
2020-08-09 09:35:17 +0200

    Migrations updated streets:other_names

--- database/migrations/2020_08_09_073221_add_other_names_column.php
+++ database/migrations/2020_08_09_073221_add_other_names_column.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStreetsTable extends Migration
+class AddOtherNamesColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateStreetsTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('streets');
-        Schema::create('streets', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('type_id');
-            $table->string('name', 100);
-            $table->timestamps();
+        Schema::table('streets', function (Blueprint $table) {
+            $table->string('other_names', 2000)->nullable()->default("");
         });
     }
 
@@ -29,6 +25,6 @@ class CreateStreetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('streets');
+        //
     }
 }
